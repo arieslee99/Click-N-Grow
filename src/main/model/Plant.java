7da2c9ef600f1b18090a0fec3798a2a5baf190e8
arieslee <arieslee@student.ubc.ca>
2023-02-06 00:39:32 -0800

    Implemented Plant class and test cases for it.

---
+++ src/main/model/Plant.java
@@ -0,0 +1,62 @@
+package model;
+
+public class Plant {
+    private String plantName;
+    private String lifeStatus;
+    private int waterCount;
+    private int fertilizerCount;
+
+    //EFFECTS: construct a plant
+    public Plant(String name, Integer waterCount, Integer fertilizerCount) {
+        this.plantName = name;
+        this.waterCount = waterCount;
+        this.fertilizerCount = fertilizerCount;
+        this.lifeStatus = "Growing!";
+    }
+
+    //MODIFIES: this
+    //EFFECTS: remove one water unit from plant's water count (wc)
+    public void waterPlant() {
+        waterCount--;
+    }
+
+    //MODIFIES: this
+    //EFFECTS: remove one fertilizer unit from the plant's fertilizer count (fc)
+    public void feedPlant() {
+        fertilizerCount--;
+    }
+
+    //MODIFIES: this
+    //EFFECTS: if plant's water count (wc) and fertilizer count (pc) are 0, set life status to "ripe";
+    //         when pc or/and wc are under 0, set to "dead";
+    //         when pc and/or wc is over 0, set to "growing"
+    public void updateLifeStatus() {
+        if (waterCount == 0 && fertilizerCount == 0) {
+            lifeStatus = "Ripe!";
+        } else if (waterCount < 0 || fertilizerCount < 0) {
+            lifeStatus = "Dead!";
+        } else {
+            lifeStatus = "Growing!";
+        }
+    }
+
+    //getters
+    public String getPlantName() {
+        return plantName;
+    }
+
+    public String getLifeStatus() {
+        return lifeStatus;
+    }
+
+    public Integer getWaterCount() {
+        return waterCount;
+    }
+
+    public Integer getFertilizerCount() {
+        return fertilizerCount;
+    }
+
+}
+
+
---
+++ src/test/model/TestPlant.java
@@ -0,0 +1,95 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestPlant {
+
+    private Plant garlic;
+    private Plant tomato;
+    private Plant rice;
+
+    @BeforeEach
+    public void setup() {
+        garlic = new Plant("Garlic", 3, 4);
+        tomato = new Plant("Tomato", 1, 1);
+        rice = new Plant("Rice", 2, 0);
+
+    }
+
+    @Test
+    public void testConstructor() {
+        assertEquals("Garlic", garlic.getPlantName());
+        assertEquals(3, garlic.getWaterCount());
+        assertEquals(4, garlic.getFertilizerCount());
+        assertEquals("Growing!", garlic.getLifeStatus());
+    }
+
+    @Test
+    public void testWaterPlantMultipleTimesAndLive() {
+        garlic.waterPlant();
+        garlic.waterPlant();
+        assertEquals(1, garlic.getWaterCount());
+        garlic.updateLifeStatus();
+        assertEquals("Growing!", garlic.getLifeStatus());
+    }
+
+    @Test
+    public void testWaterPlantMultipleTimesAndDie() {
+        garlic.waterPlant();
+        garlic.waterPlant();
+        garlic.waterPlant();
+        garlic.waterPlant();
+        assertEquals(-1, garlic.getWaterCount());
+        garlic.updateLifeStatus();
+        assertEquals("Dead!", garlic.getLifeStatus());
+
+    }
+
+    @Test
+    public void testFeedPlantMultipleTimesAndLive() {
+        garlic.feedPlant();
+        garlic.feedPlant();
+        assertEquals(2, garlic.getFertilizerCount());
+        garlic.updateLifeStatus();
+        assertEquals("Growing!", garlic.getLifeStatus());
+    }
+
+    @Test
+    public void testFeedPlantMultipleTimesAndDie() {
+        tomato.feedPlant();
+        tomato.feedPlant();
+        assertEquals(-1, tomato.getFertilizerCount());
+        tomato.updateLifeStatus();
+        assertEquals("Dead!", tomato.getLifeStatus());
+    }
+
+    @Test
+    public void testUpdateLifeStatusIsRipe() {
+        tomato.feedPlant();
+        tomato.waterPlant();
+        assertEquals(0, tomato.getWaterCount());
+        assertEquals(0, tomato.getFertilizerCount());
+        tomato.updateLifeStatus();
+        assertEquals("Ripe!", tomato.getLifeStatus());
+    }
+
+    @Test
+    public void testUpdateLifeStatusAlmostRipe() {
+        rice.waterPlant();
+        assertEquals(1, rice.getWaterCount());
+        rice.updateLifeStatus();
+        assertEquals("Growing!", rice.getLifeStatus());
+    }
+
+    @Test
+    public void testUpdateLifeStatusIsGrowing() {
+        tomato.feedPlant();
+        assertEquals(0, tomato.getFertilizerCount());
+        tomato.updateLifeStatus();
+        assertEquals("Growing!", tomato.getLifeStatus());
+    }
+
+}
